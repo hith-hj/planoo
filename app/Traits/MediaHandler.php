0d6a2790816fbce5a3da62a9bdaa141c15f6fba9
hith-hj <hjhaithem@gmail.com>
2025-10-06 16:39:23 +0300

    update syncFile function to only use php code without shell_exec

--- app/Traits/MediaHandler.php
+++ app/Traits/MediaHandler.php
@@ -101,22 +101,94 @@ trait MediaHandler
         );
     }
 
+    // private function syncImagesToPublic()
+    // {
+    //     try {
+    //         if (function_exists('shell_exec')) {
+    //             Log::info('Syncing Images');
+    //             $command = 'rsync -a --delete --inplace --quiet '
+    //                 . '/home/planoo/repositories/planoo/public/uploads/ '
+    //                 . '/home/planoo/public_html/uploads';
+    //             $guarded = "nice -n 10 ionice -c2 -n7 $command";
+    //             $result = \exec($guarded);
+    //             Log::info('Images Synced', ['result' => $result]);
+    //         } else {
+    //             Log::info('Shell is not enabled');
+    //         }
+    //     } catch (Exception $e) {
+    //         Log::info('images syncing error', ['error' => $e->getMessage()]);
+    //     }
+    // }
+
     private function syncImagesToPublic()
     {
         try {
-            if (function_exists('shell_exec')) {
-                Log::info('Syncing Images');
-                $command = 'rsync -a --delete --inplace --quiet '
-                    . '/home/planoo/repositories/planoo/public/uploads/ '
-                    . '/home/planoo/public_html/uploads';
-                $guarded = "nice -n 10 ionice -c2 -n7 $command";
-                $result = \shell_exec($guarded);
-                Log::info('Images Synced', ['result' => $result]);
-            } else {
-                Log::info('Shell is not enabled');
-            }
+            Log::info('Syncing Images');
+
+            $source = '/home/planoo/repositories/planoo/public/uploads/';
+            $destination = '/home/planoo/public_html/uploads';
+
+            $this->recursiveCopy($source, $destination);
+            $this->deleteExtraFiles($source, $destination);
+
+            Log::info('Images Synced');
         } catch (Exception $e) {
             Log::info('images syncing error', ['error' => $e->getMessage()]);
         }
+    }
+
+    private function recursiveCopy($src, $dst)
+    {
+        $dir = opendir($src);
+        @mkdir($dst, 0755, true);
+
+        while (($file = readdir($dir)) !== false) {
+            if ($file === '.' || $file === '..') continue;
+
+            $srcPath = $src . DIRECTORY_SEPARATOR . $file;
+            $dstPath = $dst . DIRECTORY_SEPARATOR . $file;
+
+            if (is_dir($srcPath)) {
+                $this->recursiveCopy($srcPath, $dstPath);
+            } else {
+                copy($srcPath, $dstPath);
+            }
+        }
+
+        closedir($dir);
+    }
+
+    private function deleteExtraFiles($src, $dst)
+    {
+        $dir = opendir($dst);
+
+        while (($file = readdir($dir)) !== false) {
+            if ($file === '.' || $file === '..') continue;
+
+            $srcPath = $src . DIRECTORY_SEPARATOR . $file;
+            $dstPath = $dst . DIRECTORY_SEPARATOR . $file;
+
+            if (!file_exists($srcPath)) {
+                if (is_dir($dstPath)) {
+                    $this->deleteDirectory($dstPath);
+                } else {
+                    unlink($dstPath);
+                }
+            } elseif (is_dir($dstPath)) {
+                $this->deleteExtraFiles($srcPath, $dstPath);
+            }
+        }
+
+        closedir($dir);
+    }
+
+    private function deleteDirectory($dir)
+    {
+        $items = array_diff(scandir($dir), ['.', '..']);
+        foreach ($items as $item) {
+            $path = $dir . DIRECTORY_SEPARATOR . $item;
+            is_dir($path) ? $this->deleteDirectory($path) : unlink($path);
+        }
+        rmdir($dir);
     }
 }
