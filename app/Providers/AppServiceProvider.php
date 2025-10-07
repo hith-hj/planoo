752bbe7457d597592207ddf42d063f8d86ca6272
hith-hj <hjhaithem@gmail.com>
2025-10-07 10:27:15 +0300

    Fix: save the 'efin' file before commiting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
