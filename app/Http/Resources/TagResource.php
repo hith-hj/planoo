2ab0d1365502ef5f56541cd6e435215ef770771e
hith-hj <hjhaithem@gmail.com>
2025-10-14 13:27:19 +0300

    update tag resource to show id with name

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -17,6 +17,7 @@ final class TagResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
         ];
     }
