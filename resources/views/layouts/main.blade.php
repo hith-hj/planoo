5ac602654d612085d888c552ea6ce3858603285b
hith-hj <hjhaithem@gmail.com>
2025-12-20 11:43:18 +0300

    wip

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Better | Leisure Centres, Gyms, Pools & Libraries</title>
+    <title>Planoo | Get Better</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <script>
