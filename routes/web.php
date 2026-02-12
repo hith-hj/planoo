0d6786065adc556a6acccf1a2482aa2625250a30
hith-hj <hjhaithem@gmail.com>
2026-02-12 13:45:22 +0300

    add names to web routes file

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/terms', 'terms')->name('terms');
 Route::get('/lang/{locale}', function ($locale) {
     if (in_array($locale, ['en', 'ar'])) {
         session()->put('locale', $locale);
