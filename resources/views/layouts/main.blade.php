33f0b28ccc17ffac1191da53456d5973a8e5f055
hith-hj <hjhaithem@gmail.com>
2026-05-13 19:52:58 +0300

    add favicon for the website

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Planoo | Get Better</title>
+    <link rel="icon" type="text/css" href="images/user-logo-new.jpeg">
+    {{-- <link rel="icon" type="text/css" href="images/partner-logo-new.jpeg"> --}}
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <script>
