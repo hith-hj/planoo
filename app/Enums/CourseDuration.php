42b0c84f41a61dbfea48faa4aac87a5adfa132f0
hith-hj <hjhaithem@gmail.com>
2026-05-19 11:16:15 +0300

    change course duration values

--- app/Enums/CourseDuration.php
+++ app/Enums/CourseDuration.php
@@ -6,9 +6,20 @@ namespace App\Enums;
 
 enum CourseDuration: int
 {
-    case short = 4;
-    case medium = 8;
-    case long = 16;
+    case one = 1;
+    case two = 2;
+    case four = 4;
+    case six = 6;
+    case eight = 8;
+    case ten = 10;
+    case twelve = 12;
+    case fourteen = 14;
+    case fifteen = 15;
+    case sixteen = 16;
+    case eighteen = 18;
+    case twenty = 20;
+    case twentyfive = 25;
+    case thirty = 30;
 
     public static function values()
     {
