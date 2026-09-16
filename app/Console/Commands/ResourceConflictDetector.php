b3978a3aba43b6ee2712ce320de58cad47b653cb
hith-hj <hjhaithem@gmail.com>
2026-09-16 10:20:10 +0300

    change resourceConflictDetector to send safeFcmPayload

--- app/Console/Commands/ResourceConflictDetector.php
+++ app/Console/Commands/ResourceConflictDetector.php
@@ -176,7 +176,7 @@ final class ResourceConflictDetector extends Command
      */
     private function outputConflicts(Model $resource, array $conflicts): void
     {
-        $resourceName = $resource->name ?? 'Resource ID: '.$resource->id;
+        $resourceName = $resource->name ?? 'Resource ID: ' . $resource->id;
 
         if (empty($conflicts)) {
             $this->info("No conflicts found for [{$resourceName}]");
@@ -213,13 +213,33 @@ final class ResourceConflictDetector extends Command
                 ],
             );
         }
+        $name = $resource->name ?? 'resource unknown';
+        $payload =  $this->safeFcmPayload($conflicts);
         $user->notify(
             'Schedule error',
-            'You have schedule conflicts',
+            "{$name} schedule conflicts",
             [
                 'type' => NotificationTypes::normal->value,
-                'conflicts' => $conflicts,
+                'conflicts' => $payload,
             ],
         );
+    }
+
+    private function safeFcmPayload(array $payload): string
+    {
+        // 1. Convert the array to a JSON string
+        $jsonString = json_encode($payload);
+
+        // 2. string length in bytes
+        $payloadBytes = strlen($jsonString);
+
+        // 3. the max limit (4 KB = 4096 bytes)
+        $maxBytes = 4096;
+
+        if ($payloadBytes <= $maxBytes) {
+            return mb_substr($jsonString, 0, 200) . '...';
+        }
+
+        return $jsonString;
     }
 }
